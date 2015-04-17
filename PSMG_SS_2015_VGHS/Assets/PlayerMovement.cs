5536edbce5a63c986c1befe2745d2b7144685cfd
Aprophis <Christoph.paulpaul@web.de>
2015-04-17 20:01:18 +0200

    Start Movement Scripting

---
+++ PSMG_SS_2015_VGHS/Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public float movementSpeed = 2f;
+    public float rotationSpeed = 4f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Move(verticalInput);
+
+        Turn(horizontalInput);
+	}
+
+    private void Turn(float horizontalInput)
+    {
+        Debug.Log("Turn(): " + horizontalInput);
+        float angle = rotationSpeed * horizontalInput;
+        gameObject.transform.Rotate(transform.up * angle);
+    }
+
+    private void Move(float verticalInput)
+    {
+        Debug.Log("Move(): " + verticalInput);
+        Vector3 newPosition = transform.forward.normalized * verticalInput * rotationSpeed * Time.deltaTime;
+        GetComponent<Rigidbody>().MovePosition(transform.position + newPosition);
+
+    }
+}
