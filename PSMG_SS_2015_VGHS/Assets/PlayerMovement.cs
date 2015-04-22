57313aa479d127be49eedb1c4d7873afa8dde0ba
Aprophis <Christoph.paulpaul@web.de>
2015-04-22 16:29:22 +0200

    Movement done...finetuning needed

--- PSMG_SS_2015_VGHS/Assets/PlayerMovement.cs
+++ PSMG_SS_2015_VGHS/Assets/PlayerMovement.cs
@@ -15,24 +15,42 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate () {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInputMouse = Input.GetAxis("Mouse X");
+        float verticalInputMouse = Input.GetAxis("Mouse Y");
 
-        Move(verticalInput);
+        Move(verticalInput, horizontalInput);
 
-        Turn(horizontalInput);
+        TurnMouse(verticalInputMouse, horizontalInputMouse);
 	}
 
-    private void Turn(float horizontalInput)
+    private void TurnMouse(float verticalInputMouse, float horizontalInputMouse)
     {
-        Debug.Log("Turn(): " + horizontalInput);
-        float angle = rotationSpeed * horizontalInput;
-        gameObject.transform.Rotate(transform.up * angle);
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            float angleX = rotationSpeed * horizontalInputMouse;
+            gameObject.transform.Rotate(transform.up * angleX);
+        }
+
+        if (Input.GetAxis("Mouse Y") != 0)
+        {
+            float angleY = rotationSpeed * verticalInputMouse;
+            //gameObject.transform.Rotate(transform.right * angleY);
+        }
     }
 
-    private void Move(float verticalInput)
+    private void Move(float verticalInput, float horizontalInput)
     {
-        Debug.Log("Move(): " + verticalInput);
-        Vector3 newPosition = transform.forward.normalized * verticalInput * rotationSpeed * Time.deltaTime;
-        GetComponent<Rigidbody>().MovePosition(transform.position + newPosition);
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            Vector3 newPosition = transform.forward.normalized * verticalInput * rotationSpeed * Time.deltaTime;
+            GetComponent<Rigidbody>().MovePosition(transform.position + newPosition);
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Vector3 newPosition = transform.right.normalized * horizontalInput * rotationSpeed * Time.deltaTime;
+            GetComponent<Rigidbody>().MovePosition(transform.position + newPosition);
+        }
 
     }
 }
