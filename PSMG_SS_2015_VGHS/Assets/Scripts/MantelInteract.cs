ef6c2befac55d8d3ef3ab04e770ece4d1c9f7957
Aprophis <Christoph.paulpaul@web.de>
2015-06-09 14:23:24 +0200

    Fixed MantelInteract
    Removed Disappearance of Mantel, added Subtitle and Movement Lock

--- PSMG_SS_2015_VGHS/Assets/Scripts/MantelInteract.cs
+++ PSMG_SS_2015_VGHS/Assets/Scripts/MantelInteract.cs
@@ -1,22 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class MantelInteract : MonoBehaviour
 {
     bool insideCollider = false;
     GameObject mantelBadewanne;
-    bool visible = true;
+    bool interacted = false;
 
     void Start() {
         mantelBadewanne = GameObject.Find("Mantel_ueber_Badewanne");
     }
     void Update()
     {
-        if (insideCollider == true && Input.GetKeyDown(KeyCode.E) && visible == true)
+        if (insideCollider == true && Input.GetKeyDown(KeyCode.E) && interacted == false)
         {
-            mantelBadewanne.GetComponent<MeshRenderer>().enabled = false;
-            visible = false;
-            //Destroy(gameObject);
+            interacted = true;
         }
     }
 
@@ -41,9 +40,21 @@ public class MantelInteract : MonoBehaviour
     {
         //GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 100, 100), "My Text");
 
-        if (insideCollider && visible == true)
+        if (insideCollider && interacted == false)
         {
             GUI.Box(new Rect(10, 10, 150, 25), "Press \"E\" to interact!");
+        }
+
+        if (insideCollider && interacted)
+        {
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
+            GUI.Box(new Rect(Screen.width / 2 - 550, Screen.height / 2 - 12, 1100, 25), "Pat Rutherford. Z.31. 12:30 Uhr… Wer ist Pat Rutherford? War ich um 12:30 bei dieser oder diesem Pat? War das heute? Was war heute? Was war gestern? WER BIN ICH? (press 'Space' to go on)");
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
+                GameObject.Find("MantelTrigger").GetComponent<SphereCollider>().enabled = false;
+                insideCollider = false;
+            }
         }
     }
 }
