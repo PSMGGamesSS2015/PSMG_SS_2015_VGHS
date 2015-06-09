bacde461917fa7c8f577932036f3f11fcc860772
Aprophis <Christoph.paulpaul@web.de>
2015-06-09 13:40:42 +0200

    Continued Scripting
    Mantel ausziehen und auf Badewanne erscheinen lassen sowie Movement Lock
    during Subtitles.

--- PSMG_SS_2015_VGHS/Assets/Scripts/WaschbeckenInteract.cs
+++ PSMG_SS_2015_VGHS/Assets/Scripts/WaschbeckenInteract.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WaschbeckenInteract : MonoBehaviour
 {
 	int interactTime = 0;
     bool insideCollider = false;
+    GameObject mantel;
+    GameObject mantelAn;
+
+    void Start()
+    {
+        mantel = GameObject.Find("Mantel_ueber_Badewanne");
+        mantelAn = GameObject.Find("Mantel");
+    }
 
 
     void Update()
@@ -56,10 +65,12 @@ public class WaschbeckenInteract : MonoBehaviour
 		//react to first interaction
 		if (interactTime == 1) 
 		{
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
 			Time.timeScale = 0;
 			GUI.Box(new Rect(Screen.width / 2 - 550, Screen.height / 2 - 12, 1100, 25), "Okay, ganz ruhig bleiben. Jetzt bloß keine Panik. Wer bist du, Frau im Spiegel? Bist du ich? Verdammt warum erkenne ich dich nicht?! Mir ist so heiß... (press 'Leer' to go on)");
 			if(Input.GetKeyDown(KeyCode.Space))
 			{
+                GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
 				Time.timeScale = 1;
 				setupJacket();
 				interactTime = 2;
@@ -69,10 +80,12 @@ public class WaschbeckenInteract : MonoBehaviour
 		//react to second interaction
 		if (interactTime == 3) 
 		{
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
 			Time.timeScale = 0;
 			GUI.Box(new Rect(Screen.width / 2 - 550, Screen.height / 2 - 12, 1100, 25), "Was zum…?! O-Oh mein… Nein, das ist… das ist Blut!!! Nicht mein Blut! Was ist denn hier los - oh Gott was habe ich getan?! (press 'Leer' to go on)");
 			if(Input.GetKeyDown(KeyCode.Space))
 			{
+                GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
 				Time.timeScale = 1;
 				interactTime = 4;
 			}
@@ -82,8 +95,8 @@ public class WaschbeckenInteract : MonoBehaviour
 	//handle taking off and rendering the jacket
 	void setupJacket()
 	{
-		Destroy (GameObject.FindGameObjectWithTag("JacketOn"));
-
+        mantelAn.GetComponent<MeshRenderer>().enabled = false;
+        mantel.GetComponent<MeshRenderer>().enabled = true;
 	}
 
 
