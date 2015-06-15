2045549e718771c3c5dc6bedf42eaccf00912a14
Sabse <sabseloliert@gmail.com>
2015-06-15 22:18:05 +0200

    Frühjahrsputz!!!
    - Badszene aufgeräumt
    - Code strukturiert (bis auf Inventar)
    - Scrip Folder aufgeräumt

---
+++ PSMG_SS_2015_VGHS/Assets/Scripts/SinkTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Look for collisions with sink
+public class SinkTrigger : MonoBehaviour {
+
+	bool isTriggered;
+
+
+	void OnTriggerEnter(Collider other){		
+		if (other.tag == "PlayerCharacter"){
+			isTriggered = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider other){
+		if (other.tag == "PlayerCharacter"){
+			isTriggered = false;
+		}
+	}
+
+	public bool sinkTriggered(){
+		return isTriggered;
+	}
+}
