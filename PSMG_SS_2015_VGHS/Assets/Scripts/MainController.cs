be686bf79e4206f4e7c45251dd539b4c91de167b
Sabse <sabseloliert@gmail.com>
2015-06-15 22:18:05 +0200

    Frühjahrsputz!!!
    - Badszene aufgeräumt
    - Code strukturiert (bis auf Inventar)
    - Scrip Folder aufgeräumt

---
+++ PSMG_SS_2015_VGHS/Assets/Scripts/MainController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+/* MAIN CONTROLLER
+ * This is the games main controlling Script. 
+ * It receives event based data from other scripts (like where collisions are triggered).
+ * Its task is to store and process this data and to communicate the results to other scripts.
+ * So every logic of the game is handled here.
+ */
+public class MainController : MonoBehaviour {
+
+	public GUIController guiController;
+	public GameObject player;
+	public GameObject sink;
+	public GameObject sinkTrigger;
+	public GameObject jacketTrigger;
+	public GameObject jacket;
+
+	int sinkCounter = 0;
+	bool noteFound = false;
+
+
+	// Use this for initialization
+	void Start () {
+		guiController.showSubtl ("entry");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		getKeyInteractions ();
+		checkGUI ();
+		checkCollisions ();
+	}
+
+	//get all the key interactions
+	void getKeyInteractions(){
+		//close a subtitle with space
+		if (Input.GetKeyDown (KeyCode.Space) && guiController.isShowing ()) {
+			guiController.unshowSubtl();
+			if(sinkCounter == 1){
+				jacket.SetActive(true);
+			}
+		}
+
+		// handle 'E' interactions
+		if (Input.GetKeyDown (KeyCode.E)) {
+			// interacted with sink
+			if (sink.GetComponent<SinkTrigger>().sinkTriggered() && sinkCounter < 2) {
+				switch (sinkCounter){
+				case 0: 
+					guiController.showSubtl("mirror1");
+					sinkCounter++;
+					break;
+				case 1:
+					guiController.showSubtl("mirror2");
+					sinkCounter++;
+					break;
+				default: break;
+				}
+			}
+			// interacted with jacket
+			else if(jacketTrigger.GetComponent<JacketTrigger>().jacketTriggered()){
+				guiController.showSubtl("paper");
+				noteFound = true;
+			}
+		}
+	}
+
+	//check if a subtitle is shown and handle player movement and interaction hint
+	void checkGUI(){
+		if (guiController.isShowing ()) {
+			player.GetComponent<FirstPersonController> ().enabled = false;
+			guiController.GetComponent<GUIController> ().unshowInteractionHint ();	
+		} else {
+			player.GetComponent<FirstPersonController> ().enabled = true;
+		}
+	}
+
+	//check if the player collides with an interactable object
+	void checkCollisions(){
+		if (sink.GetComponent<SinkTrigger> ().sinkTriggered () && guiController.isShowing () == false && sinkCounter < 2) {
+			guiController.GetComponent<GUIController> ().showInteractionHint ();			
+		}
+		else if (jacketTrigger.GetComponent<JacketTrigger> ().jacketTriggered() && noteFound == false) {
+			guiController.GetComponent<GUIController> ().showInteractionHint ();
+		} else {
+			guiController.GetComponent<GUIController> ().unshowInteractionHint ();
+		}
+	}
+}
---
+++ PSMG_SS_2015_VGHS/Assets/Scripts/Subtitle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/* SUBTITLES
+ * Handle style of Subtitle Text here.
+ */ 
+public class Subtitle : MonoBehaviour {
+
+	string show;
+	public Text subtl;
+	public string entryText;
+	public string mirrorText;
+	public string bloodyDressText;
+	public string paperText;
+	
+
+	/* Update is called once per frame
+	 * Change shown subtitles
+	 */
+	void Update () {
+		switch(show){
+		case "entry":  
+			subtl.GetComponent<Text>().text = entryText;
+			break;
+		case "mirror1": 
+			subtl.GetComponent<Text>().text = mirrorText;
+			break;
+		case "mirror2": 
+			subtl.GetComponent<Text>().text = bloodyDressText;
+			break;
+		case "paper": 
+			subtl.GetComponent<Text>().text = paperText;
+			break;
+		default: 
+			Debug.Log ("Subtitle/Update: couldnt show subtitle");
+			break;
+		}
+	}
+
+	// Setter for subtitle that needs to be shown
+	public void setKeyWord(string key){
+		show = key;
+	}
+
+
+
+
+
+
+}
