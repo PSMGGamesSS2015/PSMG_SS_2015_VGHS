bd25b06a383c6023f6196b04c3d129d63a60301d
Sabse <sabseloliert@gmail.com>
2015-07-07 14:14:39 +0200

    Startscreen überarbeitet
    MenuManager und StartscreenController zusammengeführt
    Button Activity überarbeitet

---
+++ PSMG_SS_2015_VGHS/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* GAME CONTROLLER
+ * This Controller is managing anything that happens all over the Game.
+ * It communicates with the Controllers of all Scenes and manages the communication with the UI.
+ */
+public class GameController {
+	int scene;
+
+	//constructor that recieves current scene by initialisation
+	public GameController(int sceneNum){
+		scene = sceneNum;
+		whatScene();
+	}
+
+	void whatScene(){
+		Debug.Log (scene);
+	}
+
+
+}
---
+++ PSMG_SS_2015_VGHS/Assets/Scripts/StartScreen/StartScreenController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+/* MAIN CONTROLLER
+ * This is the games main controlling Script. 
+ * It receives event based data from other scripts (like where collisions are triggered).
+ * Its task is to store and process this data and to communicate the results to other scripts.
+ * So every logic of the game is handled here.
+ * Interactions depending on the inventory like dragging & dropping hints are handled by the SlotScript and Inventory Script
+ */
+
+public class StartScreenController : MonoBehaviour {
+
+	GameController gameController;
+
+	public Canvas exitMenu;
+	public GameObject startScreenContent;
+	public Button playButton;
+	public Button optionsButton;
+	public Button exitButton;
+
+	bool buttonPressed = false;
+
+
+    // Use this for initialization
+    void Start(){ 
+		gameController = new GameController (0);
+    }
+
+    /* Update is called once per frame
+     * Toggle Button visibility here
+     */
+    void Update(){
+		if (buttonPressed) {
+			startScreenContent.SetActive(false);
+		}
+		else{
+			startScreenContent.SetActive(true);
+		}
+    }
+
+	// start game when play button pressed
+	public void OnPlayButtonPressed(){
+		buttonPressed = true;
+		GetComponent<SceneFader>().SwitchScene(1);
+	}
+
+	// handle anything belonging to option button here
+	public void OnOptionsButtonPressed(){
+		Debug.Log ("options");
+	}
+
+	// show exit menu
+	public void OnExitButtonPressed(){
+		buttonPressed = true;
+		exitMenu.enabled = true;
+	}
+
+	// quit game if yes button in exit menu was clicked
+	public void OnYesButtonPressed(){
+		Application.Quit();
+	}
+
+	// close exit menu if no was clicked
+	public void OnNoButtonPressed(){
+		buttonPressed = false;
+		exitMenu.enabled = false;
+	}
+}
