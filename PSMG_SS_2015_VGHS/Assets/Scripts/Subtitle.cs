7ab147ddab0fa03911297698bd3748ea6d9e2894
Sabse <sabseloliert@gmail.com>
2015-07-14 09:39:33 +0200

    XML File für Dialoge eingefügt
    text.xml enthält jetzt alle Texte die innerhalb des Spieles als
    Untertitel ausgegeben werden und wird im Subtitle Script geladen

--- PSMG_SS_2015_VGHS/Assets/Scripts/Subtitle.cs
+++ PSMG_SS_2015_VGHS/Assets/Scripts/Subtitle.cs
@@ -1,44 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
+using System.Xml;
+using System.IO;
 
 /* SUBTITLES
  * Handle style of Subtitle Text here.
  */ 
 public class Subtitle : MonoBehaviour {
 
-	string show;
-
-	// all strings to display are entered in Unity
+	public TextAsset textAsset;
 	public Text subtl;
-	public string entryText;
-	public string mirrorText;
-	public string bloodyDressText;
-	public string paperText;
-	public string firstTheoryText;
-	
+
+	string show;
+	string text = "";
+
+	Dictionary<string,string> lyrics = new Dictionary<string,string>();
+	Dictionary<string,string> obj;
+
+	// setup xml file first 
+	void Start(){
+		setupXmlData ();
+	}
 
 	/* Update is called once per frame
 	 * Change shown subtitles
 	 */
 	void Update () {
 		switch(show){
-		case "entry":  
-			subtl.GetComponent<Text>().text = entryText;
+		case "entry":
+			lyrics.TryGetValue("jane_bath_1", out text);
+			subtl.GetComponent<Text>().text = text;
 			break;
-		case "mirror1": 
-			subtl.GetComponent<Text>().text = mirrorText;
+		case "mirror1":
+			lyrics.TryGetValue("jane_bath_2", out text);
+			subtl.GetComponent<Text>().text = text;
 			break;
-		case "mirror2": 
-			subtl.GetComponent<Text>().text = bloodyDressText;
+		case "mirror2":
+			lyrics.TryGetValue("jane_bath_3", out text);
+			subtl.GetComponent<Text>().text = text;
 			break;
-		case "paper": 
-			subtl.GetComponent<Text>().text = paperText;
+		case "paper":
+			lyrics.TryGetValue("jane_bath_4", out text);
+			subtl.GetComponent<Text>().text = text;
 			break;
-		case "theory1": 
-			subtl.GetComponent<Text>().text = firstTheoryText;
+		case "theory1":
+			lyrics.TryGetValue("jane_bath_5", out text);
+			subtl.GetComponent<Text>().text = text;
+			break;
+		case "welcome": 
+			lyrics.TryGetValue("michael_house_1_1", out text);
+			subtl.GetComponent<Text>().text = text;
 			break;
 		default: break;
+		}
+	}
+
+	// recieve text data from xml file
+	void setupXmlData(){
+		XmlDocument xmlDoc = new XmlDocument(); 
+		xmlDoc.LoadXml (textAsset.text);
+		XmlNodeList xmlList = xmlDoc.GetElementsByTagName("ger");
+		
+		//extract child nodes into new xml node list and prepare dictionary
+		foreach(XmlNode node in xmlList){
+			XmlNodeList stringList = node.ChildNodes;
+			obj = new Dictionary<string, string>();
+			
+			//extraxt strings of child nodes and insert into dictionary
+			foreach(XmlNode childNode in stringList){
+				if(childNode.Name.ToString() != "#comment"){
+					//obj.Add(childNode.Name.ToString(), childNode.InnerText);
+					lyrics.Add(childNode.Name.ToString(), childNode.InnerText);
+				}
+			}
+			
 		}
 	}
 
