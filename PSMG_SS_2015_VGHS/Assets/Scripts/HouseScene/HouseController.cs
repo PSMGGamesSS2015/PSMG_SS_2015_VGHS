3667a9643ddbd8a3c265704729bf320562ba70e4
Sabse <sabseloliert@gmail.com>
2015-08-13 12:44:25 +0200

    Fading
    Szene wird ausgeblendet und wieder eingeblendet

--- PSMG_SS_2015_VGHS/Assets/Scripts/HouseScene/HouseController.cs
+++ PSMG_SS_2015_VGHS/Assets/Scripts/HouseScene/HouseController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class HouseController : MonoBehaviour {
 
 	public GUIController guiController;
+	public GameObject player;
 	public GameObject michaelTrigger;
 	public GameObject pianoTrigger;
 	public GameObject diningRoomTrigger1;
@@ -22,6 +23,9 @@ public class HouseController : MonoBehaviour {
 	public GameObject InteractionPanel;
 	public GameObject theory;
 	public GameObject drawer;
+	public GameObject glassTableImpression;
+
+	public bool test = false;
 
 
 	List<string> dialogsPerformed = new List<string>();
@@ -45,6 +49,7 @@ public class HouseController : MonoBehaviour {
 	bool familyInteractionDone = false;
 	bool missingPicture = false;
 	bool missingPictureFound = false;
+	bool emilyWhereabout = false;
 	bool sidetableOpen = false;
 	bool adressBookDialog = false;
 	bool adressBookFound = false;
@@ -84,126 +89,164 @@ public class HouseController : MonoBehaviour {
 			if(michaelTrigger.GetComponent<MichaelTrigger> ().michaelTriggered () && guiController.checkForPanelContent() && guiController.subtlShown !=true){
 				guiController.toggleInteractionPanel(true);
 			}
-			// handle piano dialogs
-			if(pianoTrigger.GetComponent<PianoTrigger>().pianoTriggered() && pianoCount <3 && guiController.subtlShown !=true){
-				switch (pianoCount){
-				case 0:
-					initDialog ("piano");
-					break;
+			// make sure not to check for objects that were destroyed
+			switch(actualHouseScene){
 				case 1:
-					if(daughter == false){
-						initDialog("daughter");
-						//do this if interaction was set but not done yet
-						if(familyInteractionDone){
-							guiController.closeInteractionInPanel("Kinder?");
+					// handle piano dialogs
+					if(pianoTrigger.GetComponent<PianoTrigger>().pianoTriggered() && pianoCount <3 && guiController.subtlShown !=true){
+						switch (pianoCount){
+						case 0:
+							initDialog ("piano");
+							break;
+						case 1:
+							if(daughter == false){
+								initDialog("daughter");
+								//do this if interaction was set but not done yet
+								if(familyInteractionDone){
+									guiController.closeInteractionInPanel("Kinder?");
+								}
+							}
+							else{
+								initDialog("daughter2_");
+								pianoCount++;
+							}
+							break;
+						case 2:
+							initDialog("daughter2_");
+							break;
+						default: break;
 						}
-					}
-					else{
-						initDialog("daughter2_");
 						pianoCount++;
+						
+					}
+					// handle glasstable
+					if(glassTableTrigger.GetComponent<GlassTableTrigger>().glassTableTriggered() && glassTableTriggered == false){
+						glassTableTriggered = true;
+						initDialog("glassTable");
 					}
 					break;
-				case 2:
-					initDialog("daughter2_");
+				default: break;
+			}
+
+			// handle family album 
+			if(familyAlbumTrigger.GetComponent<FamilyAlbumTrigger>().albumTriggered()){
+				switch (actualHouseScene){
+				case 1: 
+					initDialog("familyAlbum1_");
 					break;
 				default: break;
 				}
-				pianoCount++;
+			}
+			// handle noteblock
+			if(noteTrigger.GetComponent<NoteTrigger>().noteTriggered()){
+				switch (actualHouseScene){
+				case 1: 
+					guiController.toggleSubtl("noteMonolog");
+					break;
+				default: break;
+				}
+			}
+			// handle bookshelf in workroom
+			if(bookshelfTrigger.GetComponent<BookshelfTrigger>().bookshelfTriggered()){
+				switch (actualHouseScene){
+				case 1: 
+					guiController.toggleSubtl("bookshelf");
+					break;
+				default: break;
+				}
+			}
 
-			}
-			if(actualHouseScene == 1 && familyAlbumTrigger.GetComponent<FamilyAlbumTrigger>().albumTriggered()){
-				initDialog("familyAlbum1_");
-			}
-			if(actualHouseScene == 1 && noteTrigger.GetComponent<NoteTrigger>().noteTriggered()){
-				guiController.toggleSubtl("noteMonolog");
-			}
-			if(actualHouseScene == 1 && bookshelfTrigger.GetComponent<BookshelfTrigger>().bookshelfTriggered()){
-				guiController.toggleSubtl("bookshelf");
-			}
-			if(glassTableTrigger.GetComponent<GlassTableTrigger>().glassTableTriggered() && glassTableTriggered == false){
-				glassTableTriggered = true;
-				initDialog("glassTable");
-			}
-			if(sideTableTrigger.GetComponent<SideTableTrigger>().sideTableTriggered() && sidetableOpen == false && adressBookDialog == false){
-				drawer.transform.Translate(0, 0, 0.7f);
-				sidetableOpen = true;
-				initDialog("adressBook");
-			}
-			if(sideTableTrigger.GetComponent<SideTableTrigger>().sideTableTriggered() && adressBookDialog){
-				guiController.toggleAdressBook();
-				adressBookFound = true;
+			// handle side table with adress book in bedroom
+			if(sideTableTrigger.GetComponent<SideTableTrigger>().sideTableTriggered()){
+				// open sidetable
+				if (sidetableOpen == false && adressBookDialog == false){
+					drawer.transform.Translate(0, 0, 0.7f);
+					sidetableOpen = true;
+					initDialog("adressBook");
+				}
+				// interact with adressbook
+				else if(adressBookDialog){
+					guiController.toggleAdressBook();
+					adressBookFound = true;
+				}
 			}
 		}
 	}
 
 	//check if the player collides with an interactable object
 	void checkCollisions(){
-		//check if michael is triggered and interactable
-		if (michaelTrigger.GetComponent<MichaelTrigger> ().michaelTriggered () && guiController.checkForPanelContent () && guiController.isShowing () == false) {
-			guiController.toggleInteractionHint (true);
-		} 
-		// toggle dining room triggering
-		else if (diningRoom && (diningRoomTrigger1.GetComponent<DiningRoomTrigger1> ().diningRoomTriggered () || diningRoomTrigger2.GetComponent<DiningRoomTrigger2> ().diningRoomTriggered ())) {
-			guiController.toggleSubtl ("diningRoom");
-			guiController.manageInteraction ("michael_friends");
-			if (familyInteractionDone == false) {
-				guiController.manageInteraction ("michael_family");
-				familyInteractionDone = true;
+		switch (actualHouseScene) {
+		case 1: 
+			//check if michael is triggered and interactable
+			if (michaelTrigger.GetComponent<MichaelTrigger> ().michaelTriggered () && guiController.checkForPanelContent () && guiController.isShowing () == false) {
+				guiController.toggleInteractionHint (true);
+			} 
+			// toggle dining room triggering
+			else if (diningRoom && (diningRoomTrigger1.GetComponent<DiningRoomTrigger1> ().diningRoomTriggered () || diningRoomTrigger2.GetComponent<DiningRoomTrigger2> ().diningRoomTriggered ())) {
+				guiController.toggleSubtl ("diningRoom");
+				guiController.manageInteraction ("michael_friends");
+				if (familyInteractionDone == false) {
+					guiController.manageInteraction ("michael_family");
+					familyInteractionDone = true;
+				}
+				diningRoom = false;				
+			} 
+			// check if piano is triggered
+			else if (pianoTrigger.GetComponent<PianoTrigger> ().pianoTriggered () && pianoCount < 3 && guiController.isShowing () == false) {
+				guiController.toggleInteractionHint (true);
 			}
-			diningRoom = false;
-		} 
-		// check if piano is triggered
-		else if (pianoTrigger.GetComponent<PianoTrigger> ().pianoTriggered () && pianoCount < 3 && guiController.isShowing () == false) {
-			guiController.toggleInteractionHint (true);
+			// check if family album is triggered
+			else if (familyAlbumTrigger.GetComponent<FamilyAlbumTrigger> ().albumTriggered ()) {
+				guiController.toggleInteractionHint (true);
+			}
+			// check if noteblock triggered
+			else if (noteTrigger.GetComponent<NoteTrigger> ().noteTriggered () && guiController.isShowing () == false) {
+				guiController.toggleInteractionHint (true);
+			}
+			// check if jane entered wintergarden for first time
+			else if(wintergardenTrigger.GetComponent<WintergardenTrigger>().wintergardenTriggered() && wintergarden){
+				initDialog("wintergarden");
+				wintergarden = false;
+			}
+			// check if glass table was triggered
+			else if(glassTableTrigger.GetComponent<GlassTableTrigger>().glassTableTriggered() && glassTableTriggered == false){
+				guiController.toggleInteractionHint(true);
+			}
+			// check if player entered childsroom
+			else if(childsroom && childsroomTrigger.GetComponent<ChildsroomTrigger>().childsroomTriggered()){
+				initDialog("childsroom");
+				childsroom = false;
+			}
+			// check if player entered guestroom
+			else if(guestroom && guestroomTrigger.GetComponent<GuestroomTrigger>().guestroomTriggered()){
+				initDialog("guestroom");
+				guestroom = false;
+			}
+			// check if player entered workroom
+			else if (workroom && workroomTrigger.GetComponent<WorkroomTrigger>().workroomTriggered()){
+				guiController.toggleSubtl("workroom");
+				workroom = false;
+			}
+			// check if player is nearby bookshelf in workroom
+			else if (bookshelfTrigger.GetComponent<BookshelfTrigger>().bookshelfTriggered()&& guiController.isShowing () == false){
+				guiController.toggleInteractionHint(true);
+			}
+			// check if player entered bedroom
+			else if (bedroom && bedroomTrigger.GetComponent<BedroomTrigger>().bedroomTriggered()){
+				initDialog("bedroom");
+				bedroom = false;
+			}
+			// check if player is near sidetable
+			else if (sideTableTrigger.GetComponent<SideTableTrigger>().sideTableTriggered() && guiController.isShowing () == false){
+				guiController.toggleInteractionHint (true);
+			}
+			else {
+				guiController.toggleInteractionHint (false);
+			}
+			break;
+		default: break;
 		}
-		// check if family album is triggered
-		else if (familyAlbumTrigger.GetComponent<FamilyAlbumTrigger> ().albumTriggered ()) {
-			guiController.toggleInteractionHint (true);
-		}
-		// check if noteblock triggered
-		else if (noteTrigger.GetComponent<NoteTrigger> ().noteTriggered () && guiController.isShowing () == false) {
-			guiController.toggleInteractionHint (true);
-		}
-		// check if jane entered wintergarden for first time
-		else if(wintergardenTrigger.GetComponent<WintergardenTrigger>().wintergardenTriggered() && wintergarden){
-			initDialog("wintergarden");
-			wintergarden = false;
-		}
-		// check if glass table was triggered
-		else if(glassTableTrigger.GetComponent<GlassTableTrigger>().glassTableTriggered() && glassTableTriggered == false){
-			guiController.toggleInteractionHint(true);
-		}
-		// check if player entered childsroom
-		else if(childsroom && childsroomTrigger.GetComponent<ChildsroomTrigger>().childsroomTriggered()){
-			initDialog("childsroom");
-			childsroom = false;
-		}
-		// check if player entered guestroom
-		else if(guestroom && guestroomTrigger.GetComponent<GuestroomTrigger>().guestroomTriggered()){
-			initDialog("guestroom");
-			guestroom = false;
-		}
-		// check if player entered workroom
-		else if (workroom && workroomTrigger.GetComponent<WorkroomTrigger>().workroomTriggered()){
-			guiController.toggleSubtl("workroom");
-			workroom = false;
-		}
-		// check if player is nearby bookshelf in workroom
-		else if (bookshelfTrigger.GetComponent<BookshelfTrigger>().bookshelfTriggered()&& guiController.isShowing () == false){
-			guiController.toggleInteractionHint(true);
-		}
-		// check if player entered bedroom
-		else if (bedroom && bedroomTrigger.GetComponent<BedroomTrigger>().bedroomTriggered()){
-			initDialog("bedroom");
-			bedroom = false;
-		}
-		// check if player is near sidetable
-		else if (sideTableTrigger.GetComponent<SideTableTrigger>().sideTableTriggered() && guiController.isShowing () == false){
-			guiController.toggleInteractionHint (true);
-		}
-		else {
-			guiController.toggleInteractionHint (false);
-		}
+
 	}
 
 	// check for new interactions in interaction panel
@@ -326,6 +369,7 @@ public class HouseController : MonoBehaviour {
 			// add hint about emilies whereabout
 			else if(actualSubtl.Substring(0, actualSubtl.Length-1).Equals("childsroom")){
 				insertIntoInventory("emilyWhereabout");
+				emilyWhereabout = true;
 			}
 			// register that dialog about the adress book is done
 			else if (actualSubtl.Substring(0, actualSubtl.Length-1).Equals("adressBook")){
@@ -335,13 +379,14 @@ public class HouseController : MonoBehaviour {
 			else if(actualSubtl.Substring(0, actualSubtl.Length-1).Equals("scene1Ending")){
 				scene1EndingDialog = true;
 				insertIntoInventory("pills");
+				arrangeNextScene(actualHouseScene);
 			}
 			// reset dialog data for new dialog
 			dialogCount = 1;
 			actualDialog = "";
 			dialog = false;
 		} 
-		// insert hint about picture during dialog (this is a special case)
+		// insert hint about picture during dialog (this is a special case where the hint is added during dialog)
 		else if(actualSubtl.Equals("daughter2_2")){
 			insertIntoInventory("picture");
 			guiController.toggleSubtl (actualSubtl);
@@ -385,8 +430,34 @@ public class HouseController : MonoBehaviour {
 	// check if all necessary interactions are done to end a scene
 	void checkSceneEnding (){
 		//Ending conditions for first Scene in house
-		if(theory2Registered && missingPictureFound && glassTableTriggered && friends && family && adressBookFound && guiController.isShowing() == false && scene1EndingDialog == false){
+		if(theory2Registered && missingPictureFound && glassTableTriggered && friends && family && adressBookFound && guiController.isShowing() == false && scene1EndingDialog == false && emilyWhereabout){
 			initDialog("scene1Ending");
 		}
+		// move player to bed
+		if (GetComponent<SceneFader> ().checkFade() && actualHouseScene > 1) {
+			GetComponent<SceneFader> ().forceFadeIn();
+		}
+	}
+
+	// delete or enable objects, trigger etc. as needed in next scene
+	void arrangeNextScene(int scene){
+		GetComponent<SceneFader> ().SwitchScene (3);
+		switch (scene) {
+		case 1:
+			Destroy (glassTableImpression);
+			Destroy(wintergardenTrigger);
+			Destroy(glassTableTrigger);
+			Destroy (pianoTrigger);
+			Destroy(diningRoomTrigger1);
+			Destroy (childsroomTrigger);
+			Destroy (workroomTrigger);
+			Destroy (guestroomTrigger);
+			Destroy (bedroomTrigger);
+			diningRoom = true;
+			actualHouseScene++;
+			break;
+		default: break;
+		}
+
 	}
 }
