e3d04ad07be84cfa014b595b7f89d271af73a187
Sabse <sabseloliert@gmail.com>
2015-08-29 17:14:00 +0200

    Imported Soundfiles

---
+++ PSMG_SS_2015_VGHS/Assets/Scripts/AudioController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour {
+
+	public GameObject backgroundMusic;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		toggleBackgroundMusic();
+	}
+
+	// manage backgroundmusic
+	void toggleBackgroundMusic(){
+		switch (Application.loadedLevel) {
+		case 0:
+
+			break;
+		case 1:
+
+			break;
+		case 2:
+
+			break;
+		}
+	}
+}
